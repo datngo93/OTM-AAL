539c63ca895f6708d103050214525a34fbba4e6a
Dat <v1t0ry@users.noreply.github.com>
2021-12-10 21:37:27 +0900

    Update OTM_demo.m

--- OTM_demo.m
+++ OTM_demo.m
@@ -13,16 +13,16 @@ synthetic = imread('synthetic_scene.png');
 [j3,t3,ten3] = OTM(synthetic);
 
 figure;
-hazy = double(hazy(1:size(j1,1),1:size(j1,2),:))/255;
+hazy = double(hazy)/255;
 subplot(211); imshow([hazy,j1]);
 subplot(212); imshow([t1,ten1],[]); colormap(jet(256));
 
 figure;
-yellowdust = double(yellowdust(1:size(j2,1),1:size(j2,2),:))/255;
+yellowdust = double(yellowdust)/255;
 subplot(211); imshow([yellowdust,j2]);
 subplot(212); imshow([t2,ten2],[]); colormap(jet(256));
 
 figure;
-synthetic = double(synthetic(1:size(j3,1),1:size(j3,2),:))/255;
+synthetic = double(synthetic)/255;
 subplot(211); imshow([synthetic,j3]);
 subplot(212); imshow([t3,ten3],[]); colormap(jet(256));
